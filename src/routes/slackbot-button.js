9d1e628cf91aed08d3d8593a261b38fd26f9b3a5
Ram Prasad Agarwal <ramprasadagarwal313@gmail.com>
2018-03-18 19:59:07 +0530

    modify

--- src/routes/slackbot-button.js
+++ src/routes/slackbot-button.js
@@ -4,35 +4,36 @@ module.exports = {
   method: 'POST',
   path: '/slackbot-button',
   handler: (request, response) => {
-    const requestJSON = JSON.parse(request.payload);
-    console.log('incoming request', requestJSON);
-    const payload = requestJSON.actions;
+    // const requestJSON = JSON.parse(request);
+    console.log('incoming request', request.payload);
+    const reqJSON = JSON.parse(request.payload);
+    const payload = reqJSON.actions;
     const status = payload.name;
     const eventid = payload.value;
-    const userid = requestJSON.user.name;
+    // const userid = request.payload.user.name;
     console.log('status', status);
     console.log('eventid', eventid);
-    console.log('userid', userid);
-    models.responses.update({
-      status,
-    }, {
-      where: {
-        [models.Sequelize.Op.and]: [{ userid }, { eventid }],
-      },
-    })
-      .then(() => {
-        models.events.findOne({
-          where: {
-            userid,
-          },
-        })
-          .then(result1 => result1.dataValues)
-          .then((result2) => {
-            const finalMessage = `You have ${status} the ${result2.type} invitation on ${result2.venue} date: ${result2.date} time: ${result2.time}`;
-            response(finalMessage);
-          });
-        response();
-      });
+    // console.log('userid', userid);
+    // models.responses.update({
+    //   status,
+    // }, {
+    //   where: {
+    //     [models.Sequelize.Op.and]: [{ userid }, { eventid }],
+    //   },
+    // })
+    //   .then(() => {
+    //     models.events.findOne({
+    //       where: {
+    //         userid,
+    //       },
+    //     })
+    //       .then(result1 => result1.dataValues)
+    //       .then((result2) => {
+    //         const finalMessage = `You have ${status} the ${result2.type} invitation on ${result2.venue} date: ${result2.date} time: ${result2.time}`;
+    //         response(finalMessage);
+    //       });
+    //     response();
+    //   });
   },
 };
 
