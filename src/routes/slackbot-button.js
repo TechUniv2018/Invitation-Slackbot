d24872c4ad60733bcff1331d70e0dc333eef66ac
Ram Prasad Agarwal <ramprasadagarwal313@gmail.com>
2018-03-18 20:08:15 +0530

    Formattify

--- src/routes/slackbot-button.js
+++ src/routes/slackbot-button.js
@@ -5,35 +5,35 @@ module.exports = {
   path: '/slackbot-button',
   handler: (request, response) => {
     // const requestJSON = JSON.parse(request);
-    console.log('incoming request', request.payload);
-    const reqJSON = JSON.parse(request.payload);
-    const payload = reqJSON.actions;
-    const status = payload.name;
-    const eventid = payload.value;
-    // const userid = request.payload.user.name;
+    // console.log('incoming request', request.payload);
+    const reqJSON = JSON.parse(request.payload.payload);
+    const { actions } = reqJSON;
+    const status = actions[0].name;
+    const eventid = actions[0].value;
+    const userid = reqJSON.user.name;
     console.log('status', status);
     console.log('eventid', eventid);
-    // console.log('userid', userid);
-    // models.responses.update({
-    //   status,
-    // }, {
-    //   where: {
-    //     [models.Sequelize.Op.and]: [{ userid }, { eventid }],
-    //   },
-    // })
-    //   .then(() => {
-    //     models.events.findOne({
-    //       where: {
-    //         userid,
-    //       },
-    //     })
-    //       .then(result1 => result1.dataValues)
-    //       .then((result2) => {
-    //         const finalMessage = `You have ${status} the ${result2.type} invitation on ${result2.venue} date: ${result2.date} time: ${result2.time}`;
-    //         response(finalMessage);
-    //       });
-    //     response();
-    //   });
+    console.log('userid', userid);
+    models.responses.update({
+      status,
+    }, {
+      where: {
+        [models.Sequelize.Op.and]: [{ userid }, { eventid }],
+      },
+    })
+      .then(() => {
+        models.events.findOne({
+          where: {
+            userid,
+          },
+        })
+          .then(result1 => result1.dataValues)
+          .then((result2) => {
+            const finalMessage = `You have ${status} the ${result2.type} invitation on ${result2.venue} date: ${result2.date} time: ${result2.time}`;
+            response(finalMessage);
+          });
+        response();
+      });
   },
 };
 
