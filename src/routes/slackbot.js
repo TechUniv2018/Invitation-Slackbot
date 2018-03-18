d5f22a8b14bb6841f2a40646fd2bdfeea5a657dc
Ram Prasad Agarwal <ramprasadagarwal313@gmail.com>
2018-03-18 20:53:15 +0530

    adding reminder

--- src/routes/slackbot.js
+++ src/routes/slackbot.js
@@ -16,7 +16,7 @@ module.exports = {
     const time = messageBody[messageBody.indexOf('time:') + 1];
     const tempVenue = request.payload.text.split('venue: ');
     const venue = tempVenue[1].split(' date:')[0];
-    const responseMessage = `Hey! you have been invited for a ${type} in ${venue} date: ${date} time: ${time}`;
+    const responseMessage = '/remind @parth water 8:55';
     console.log('type: ', type);
     console.log('venue: ', venue);
     console.log('date: ', date);
@@ -37,25 +37,25 @@ module.exports = {
         eventId = result.dataValues.eventid;
         const message = [
           {
-            text: '/remind @parth water 8:52',
+            text: 'Would you like to join',
             fallback: "Shame... buttons aren't supported in this land",
             callback_id: 'event-id-here',
-            // color: '#3AA3E3',
-            // attachment_type: 'default',
-            // actions: [
-            //   {
-            //     name: 'Accepted',
-            //     text: 'Accept',
-            //     type: 'button',
-            //     value: eventId,
-            //   },
-            //   {
-            //     name: 'Rejected',
-            //     text: 'Reject',
-            //     type: 'button',
-            //     value: eventId,
-            //   },
-            // ],
+            color: '#3AA3E3',
+            attachment_type: 'default',
+            actions: [
+              {
+                name: 'Accepted',
+                text: 'Accept',
+                type: 'button',
+                value: eventId,
+              },
+              {
+                name: 'Rejected',
+                text: 'Reject',
+                type: 'button',
+                value: eventId,
+              },
+            ],
           },
         ];
         const promiseArr = [];
@@ -63,7 +63,7 @@ module.exports = {
           const urlparam = {
             token: key,
             channel: id,
-            attachments: JSON.stringify(message),
+            // attachments: JSON.stringify(message),
             text: responseMessage,
           };
           const qs = querystring.stringify(urlparam);
