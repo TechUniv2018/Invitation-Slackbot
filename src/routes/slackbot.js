7b26edb34d25a04c500a70dfb14bbb139c22b0ee
Vishal Vasnani <vishalvasnani123@gmail.com>
2018-03-18 18:20:41 +0530

    Refactor

--- src/routes/slackbot.js
+++ src/routes/slackbot.js
@@ -1,6 +1,6 @@
 const req = require('request');
 const querystring = require('querystring');
-const bot = require('../helpers/bot');
+// const bot = require('../helpers/bot');
 const key = require('../constants/keys');
 
 const message = [
@@ -33,25 +33,25 @@ module.exports = {
   handler: (request, response) => {
     const recipients = new Set(request.payload.text.split(/[ ]+/)
       .filter(e => e[0] === '@'));
-      const messageBody = request.payload.text.split(/[ ]/);
-      const type = messageBody[messageBody.indexOf('type:')+1];
-      const time = messageBody[messageBody.indexOf('at:')+1];
-      const tempVenue = request.payload.text.split('venue: ');
-      const venue = tempVenue[1].split(' at:');
-      console.log('type, venue and time ',type,venue, time);
-      const recArr = Array.from(recipients);
-      const inviteMessage = 'Hey! you have been invited for a '+ type +' in ' + venue;
-      recArr.forEach((id) => {
-        const urlparam = {
-          token: key,
-          channel: id,
-          attachments: JSON.stringify(message),
-          text: inviteMessage,
-        };
+    const messageBody = request.payload.text.split(/[ ]/);
+    const type = messageBody[messageBody.indexOf('type:') + 1];
+    const time = messageBody[messageBody.indexOf('at:') + 1];
+    const tempVenue = request.payload.text.split('venue: ');
+    const venue = tempVenue[1].split(' at:');
+    console.log('type, venue and time ', type, venue, time);
+    const recArr = Array.from(recipients);
+    const inviteMessage = `Hey! you have been invited for a ${type} in ${venue}`;
+    recArr.forEach((id) => {
+      const urlparam = {
+        token: key,
+        channel: id,
+        attachments: JSON.stringify(message),
+        text: inviteMessage,
+      };
       const qs = querystring.stringify(urlparam);
-      const path_to_call = `http://slack.com/api/chat.postMessage?${qs}`;
-      req(path_to_call, (error, response, body) => {
-        if (!error && response.statusCode == 200) {
+      const pathToCall = `http://slack.com/api/chat.postMessage?${qs}`;
+      req(pathToCall, (error, res) => {
+        if (!error && res.statusCode === 200) {
           console.log('Success');
         } else {
           console.log(error);
