6972d3873dbbe9242a254bd9ccbcefc1bc999f51
parth <srivastavaparth2016@gmail.com>
2018-03-19 01:26:05 +0530

    changes for the reminder

--- src/routes/slackbot-button.js
+++ src/routes/slackbot-button.js
@@ -1,10 +1,14 @@
 const models = require('../../models');
+const key2 = require('../constants/key2');
+const querystring = require('querystring');
+const getAllUsers = require('../helpers/getAllUsers');
+const rp = require('request-promise');
+
 
 module.exports = {
   method: 'POST',
   path: '/slackbot-button',
   handler: (request, response) => {
-    console.log(request);
     // const requestJSON = JSON.parse(request);
     // console.log('incoming request', request.payload);
     const reqJSON = JSON.parse(request.payload.payload);
@@ -31,7 +35,29 @@ module.exports = {
           .then(result1 => result1.dataValues)
           .then((result2) => {
             const finalMessage = `You have ${status} the ${result2.title} invitation at ${result2.venue} date: ${result2.date} time: ${result2.time}`;
-            response(finalMessage);
+            if (status === 'Accepted') {
+              getAllUsers().then((values) => {
+                const newId = values[userid];
+                const urlparam2 = {
+                  token: key2,
+                  user: newId,
+                  // attachments: JSON.stringify(message),
+                  time: result2.time,
+                  // command: '/remind',
+                  text: result2.title,
+                };
+                const qs2 = querystring.stringify(urlparam2);
+                console.log(qs2);
+                const options = {
+                  method: 'POST',
+                  url: `https://slack.com/api/reminders.add?${qs2}`,
+                };
+                rp(options).then(() => {
+                  console.log('####pushed');
+                  response(finalMessage);
+                });
+              });
+            }
           });
       });
   },
