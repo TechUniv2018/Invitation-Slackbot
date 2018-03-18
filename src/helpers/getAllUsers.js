4cbcf6223a364c61177d61bd8b2d41f228a13729
Vishal Vasnani <vishalvasnani123@gmail.com>
2018-03-19 00:29:25 +0530

    Added all users

---
+++ src/helpers/getAllUsers.js
@@ -0,0 +1,26 @@
+const Bot = require('slackbots');
+const key = require('../constants/keys');
+
+// create a bot
+const settings = {
+  token: key,
+  name: 'Invitations',
+};
+const bot = new Bot(settings);
+
+const allUsers = [];
+const loadBotUser = () => {
+  const promise = new Promise((resolve) => {
+    bot.getUsers()
+      .then(result => result.members.filter(eachUser =>
+        eachUser.is_bot === false && eachUser.id !== 'USLACKBOT'))
+      .then(users => users.forEach((user) => {
+        allUsers[user.id] = user.name;
+      })).then(() => {
+        resolve(allUsers);
+      });
+  });
+  return promise;
+};
+
+module.exports = loadBotUser();
