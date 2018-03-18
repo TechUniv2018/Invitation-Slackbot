1d09f6ed63060501198d91d5a7a478b66dbc65ad
parth <srivastavaparth2016@gmail.com>
2018-03-18 22:33:00 +0530

    changes

--- src/routes/slackbot.js
+++ src/routes/slackbot.js
@@ -1,8 +1,6 @@
 const rp = require('request-promise');
 const querystring = require('querystring');
-// const bot = require('../helpers/bot');
 const key = require('../constants/keys');
-const models = require('../../models');
 
 module.exports = {
   method: 'POST',
@@ -10,18 +8,18 @@ module.exports = {
   handler: (request, response) => {
     const urlparam = {
       token: key,
-      user: 'U9R2YTYUR',
+      user: 'U9SKHDACE',
       // attachments: JSON.stringify(message),
       time: '11:00pm',
       // command: '/remind',
       text: 'eat a banana',
     };
     const qs = querystring.stringify(urlparam);
+    console.log(qs);
     const options = {
       method: 'POST',
       url: `https://slack.com/api/reminders.add?${qs}`,
     };
-      // const pathToCall = ;
     rp(options).then(() => {
       response('Invitation Sent Successfully!');
     });
